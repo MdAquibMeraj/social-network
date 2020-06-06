6bca09e2d5b712e9de803ad8ea7c497e5937e934
Md Aquib Meraj <kamranmeraj6789@gmail.com>
2020-06-06 16:57:35 +0700

    profile view created..

--- src/components/AboutYou.js
+++ src/components/AboutYou.js
@@ -5,42 +5,48 @@ class AboutYou extends React.Component {
 	renderProfile = () => {
 		return (
 			<div className="aboutyou p-md-3">
-				<div className="row">
-					<div className="col-12">
-						<div className="card card-inverse" style={{ backgroundColor: '#e9eaed', borderColor: '#e3e3e3' }}>
-							<div className="card-block">
-								<div className="row">
+				<div className="container">
+					<div className="">
+							{/* <div className="card card-inverse col-6" style={{ backgroundColor: '#e9eaed', borderColor: '#e3e3e3' }}>
+								<div className="card-block row m-3">
 									<div className="col-md-4 col-sm-4 text-center">
-										<img className="btn-md m-3" src={loginCard} alt="" style={{ borderRadius: '50%', width: '40%' }} />
+										<img className="btn-md" src={loginCard} alt="" style={{ borderRadius: '50%', width: '100%' }} />
 									</div>
-									<div className="col-md-8 col-sm-8 my-3">
+									<div className="col-md-8 col-sm-8 text-left pl-5">
 										<h2 className="card-title"><strong>Md. Aquib Meraj</strong></h2>
 										<p className="card-text">Bachelor of Engineering, 4th Year </p>
 										<p className="card-text">Computer Science </p>
 										<p className="card-text"><strong>Interest: </strong> Website development </p>
 										<p className="card-text"><strong>Skills: </strong> UI development </p>
-										<p>
-											<span className="badge bg-primary mx-1">HTML5</span>
-											<span className="badge bg-info mx-1">CSS3</span>
-											<span className="badge bg-warning mx-1">Bootstrap</span>
-											<span className="badge bg-success mx-1">ReactJs</span>
-										</p>
 									</div>
-									<div className="col-md-4 col-sm-4 text-center">
-										<h2><strong> 5.2K </strong></h2>
-										<p><small>Articles</small></p>
-										<button className="btn btn-primary btn-block btn-md"><span className="fa fa-facebook-square"></span> Like  </button>
+									<div className="col-12 pt-4">
+										<span class="badge badge-primary mx-1">HTML5</span>
+										<span class="badge badge-secondary mx-1">JavaScript</span>
+										<span class="badge badge-success mx-1">CSS3</span>
+										<span class="badge badge-danger mx-1">Bootstrap</span>
+										<span class="badge badge-warning mx-1">ReactJs</span>
+										<span class="badge badge-info mx-1">EcmaScript 6</span>
+										<span class="badge badge-dark mx-1">Node Js</span>
 									</div>
-									<div className="col-md-4 col-sm-4 text-center">
-										<h2><strong>1.4K</strong></h2>
-										<p><small>Following</small></p>
-										<button className="btn btn-success btn-block btn-md"><span className="fa fa-twitter-square"></span> Tweet </button>
-									</div>
-									<div className="col-md-4 col-sm-4 text-center">
-										<h2><strong>3.8K</strong></h2>
-										<p><small>Followers</small></p>
-										<button type="button" className="btn btn-danger btn-block btn-md"><span className="fa fa-google-plus-square"></span> Follow </button>
-									</div>
+								</div>
+						</div> */}
+						<div className="card" style={{width: '25rem'}}>
+							<img className="card-img-top" style={{backgroundSize: 'cover', height: '25rem', backgroundRepeat: 'no-repeat', backgroundPosition: '50% 50%',   backgroundImage: `url(${loginCard})` }} alt=""/>
+							<div className="card-body text-left">
+								<h5 className="card-title">Md Aquib Meraj</h5>
+								<p className="card-text">Final year </p>
+								<p className="card-text">Bachelor of Engineering </p>
+								<p className="card-text">Computer Science </p>
+								<p className="card-text"><strong>Interest: </strong> Website development </p>
+								<p className="card-text"><strong>Skills: </strong> UI development </p>
+								<div className="pt-2">
+									<span class="badge badge-primary mx-1">HTML5</span>
+									<span class="badge badge-secondary mx-1">JavaScript</span>
+									<span class="badge badge-success mx-1">CSS3</span>
+									<span class="badge badge-danger mx-1">Bootstrap</span>
+									<span class="badge badge-warning mx-1">ReactJs</span>
+									<span class="badge badge-info mx-1">EcmaScript 6</span>
+									<span class="badge badge-dark mx-1">Node Js</span>
 								</div>
 							</div>
 						</div>
