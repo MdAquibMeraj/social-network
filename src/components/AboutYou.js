6761c0647cfe0ad2fd266f85670b77c228aa2480
Md Aquib Meraj <kamranmeraj6789@gmail.com>
2020-06-06 17:09:08 +0700

    alignment sorted..

--- src/components/AboutYou.js
+++ src/components/AboutYou.js
@@ -6,7 +6,8 @@ class AboutYou extends React.Component {
 		return (
 			<div className="aboutyou p-md-3">
 				<div className="container">
-					<div className="">
+					<div className="row">
+						<div className="col-md-4 col-sm-12">
 							{/* <div className="card card-inverse col-6" style={{ backgroundColor: '#e9eaed', borderColor: '#e3e3e3' }}>
 								<div className="card-block row m-3">
 									<div className="col-md-4 col-sm-4 text-center">
@@ -30,25 +31,29 @@ class AboutYou extends React.Component {
 									</div>
 								</div>
 						</div> */}
-						<div className="card" style={{width: '25rem'}}>
-							<img className="card-img-top" style={{backgroundSize: 'cover', height: '25rem', backgroundRepeat: 'no-repeat', backgroundPosition: '50% 50%',   backgroundImage: `url(${loginCard})` }} alt=""/>
-							<div className="card-body text-left">
-								<h5 className="card-title">Md Aquib Meraj</h5>
-								<p className="card-text">Final year </p>
-								<p className="card-text">Bachelor of Engineering </p>
-								<p className="card-text">Computer Science </p>
-								<p className="card-text"><strong>Interest: </strong> Website development </p>
-								<p className="card-text"><strong>Skills: </strong> UI development </p>
-								<div className="pt-2">
-									<span class="badge badge-primary mx-1">HTML5</span>
-									<span class="badge badge-secondary mx-1">JavaScript</span>
-									<span class="badge badge-success mx-1">CSS3</span>
-									<span class="badge badge-danger mx-1">Bootstrap</span>
-									<span class="badge badge-warning mx-1">ReactJs</span>
-									<span class="badge badge-info mx-1">EcmaScript 6</span>
-									<span class="badge badge-dark mx-1">Node Js</span>
+							<div className="card">
+								<img className="card-img-top" style={{ backgroundSize: 'cover', height: '25rem', backgroundRepeat: 'no-repeat', backgroundPosition: '50% 50%', backgroundImage: `url(${loginCard})` }} alt="" />
+								<div className="card-body text-left">
+									<h5 className="card-title">Md Aquib Meraj</h5>
+									<p className="card-text">Final year </p>
+									<p className="card-text">Bachelor of Engineering </p>
+									<p className="card-text">Computer Science </p>
+									<p className="card-text"><strong>Interest: </strong> Website development </p>
+									<p className="card-text"><strong>Skills: </strong> UI development </p>
+									<div className="pt-2">
+										<span class="badge badge-primary mx-1">HTML5</span>
+										<span class="badge badge-secondary mx-1">JavaScript</span>
+										<span class="badge badge-success mx-1">CSS3</span>
+										<span class="badge badge-danger mx-1">Bootstrap</span>
+										<span class="badge badge-warning mx-1">ReactJs</span>
+										<span class="badge badge-info mx-1">EcmaScript 6</span>
+										<span class="badge badge-dark mx-1">Node Js</span>
+									</div>
 								</div>
 							</div>
+						</div>
+						<div className="col-md-8 col-sm-12">
+							<h3>My name is Aquib meraj</h3>
 						</div>
 					</div>
 				</div>
