d425e37462d44c879f4fbdf819ef6abb21449269
Md Aquib Meraj <kamranmeraj6789@gmail.com>
2020-06-07 19:53:02 +0700

    moved files from assets to public folder. dynamic rendering done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,6 @@
 import React, { Fragment } from 'react';
 
 import bloglistJson from '../json/bloglist.json';
-import blogPostThum1 from '../assets/blogs/blog-post-thumb-1.jpg';
-import blogPostThum2 from '../assets/blogs/blog-post-thumb-2.jpg';
-import blogPostThum3 from '../assets/blogs/blog-post-thumb-3.jpg';
-import blogPostThum4 from '../assets/blogs/blog-post-thumb-4.jpg';
-import blogPostThum5 from '../assets/blogs/blog-post-thumb-5.jpg';
-import blogPostThum6 from '../assets/blogs/blog-post-thumb-6.jpg';
-import blogPostThum7 from '../assets/blogs/blog-post-thumb-7.jpg';
-import blogPostThum8 from '../assets/blogs/blog-post-thumb-8.jpg';
-import blogPostThum9 from '../assets/blogs/blog-post-thumb-9.jpg';
-import blogPostThum10 from '../assets/blogs/blog-post-thumb-10.jpg';
-import blogPostThum11 from '../assets/blogs/blog-post-thumb-11.jpg';
-import blogPostThum12 from '../assets/blogs/blog-post-thumb-12.jpg';
 
 import profileImg from '../assets/blogs/profile.png';
 import { faHistory, faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -90,21 +78,21 @@ class Home extends React.Component {
 	}
 
 	renderBlogList = () => {
-		const { blogs } = this.props;
-		console.log(blogs);
-		return (
-			<div className="item mb-5 text-left">
-				<div className="media">
-					<img className="mr-3 img-fluid post-thumb d-none d-md-flex" src={blogPostThum1} alt="" />
-					<div className="media-body">
-						<h3 className="title mb-1"><a href="blog-post.html">Why Every Developer Should Have A Blog</a></h3>
-						<div className="meta mb-1"><span className="date">Published 2 days ago</span><span className="time">5 min read</span><span className="comment"><a href="#">8 comments</a></span></div>
-						<div className="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
-						<a className="more-link" href="blog-post.html">Read more →</a>
-					</div>
-				</div>
-			</div>					
-		);
+		if (bloglistJson) {
+			return bloglistJson.map(({ title, date, description, thumbnail }) => (
+					<div className="item mb-5 text-left">
+						<div className="media">
+							<img className="mr-3 img-fluid post-thumb d-none d-md-flex" src={thumbnail} alt="" />
+							<div className="media-body">
+								<h3 className="title mb-1"><a href="blog-post.html">{title}</a></h3>
+								<div className="meta mb-1"><span className="date">{date}</span><span className="time">5 min read</span><span className="comment"><a href="#">8 comments</a></span></div>
+								<div className="intro">{description}</div>
+								<a className="more-link" href="blog-post.html">Read more →</a>
+							</div>
+						</div>
+					</div>					
+				));
+		}
 	}
 	
 }
