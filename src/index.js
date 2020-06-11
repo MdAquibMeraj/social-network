f7aa3f4cc9a29d81c6fc4c84efd0d9eeb69cc058
Mohsin Tariq <mynameismohsintariq@gmail.com>
2020-06-11 10:32:18 +0530

    multi theme selection done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery';
 import 'popper.js';
 import 'bootstrap/dist/js/bootstrap';
 import { applyMiddleware, createStore } from 'redux';
